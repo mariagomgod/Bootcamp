482931bf28f17b65da47776eb4937ebd00083795
Maria Gomez <maria_gg86@yahoo.es>
2021-04-15 20:27:11 +0200

    apartado 1 ejercicio 6 JavaScript Json finalizado y prueba ejercicios 4 y 5 modificados

--- Tema_5_JavaScript/ejercicios/ejercicio4 /js/prueba_eje_4.js
+++ Tema_5_JavaScript/ejercicios/ejercicio4 /js/prueba_eje_4.js
@@ -19,50 +19,5 @@ const books = [
     new Book(10, "La danza de los tulipanes", "Ibon Martín", 20, 17),
 ];
 
-const tBody = document.getElementById("information");
 
-function removeBook(bookToRemove) {
-    // me aseguro eliminar el libro correcto
-    const bookFound = books.find(book => book.title === bookToRemove.title && book.author === bookToRemove.author);
-    const index = books.indexOf(bookFound); // localizo el objeto book dentro del array
-    books.splice(index, 1); // elimino un elemento empezando desde la posición index
-    updateTable(); // vuelvo a pintar la tabla
-}
-
-function updateTable() {
-
-    tBody.innerHTML = ""; // para vaciar la tabla
-
-    for (const book of books) {
-        const newRow = document.createElement("tr"); // creo la fila
-        tBody.appendChild(newRow); // añado la fila al tbody
-
-        for (const propertyName in book) {
-            const column = document.createElement("td"); // creo la columna
-            newRow.appendChild(column); // añado la columna a la fila correspondiente
-            column.textContent = book[propertyName]; // pinto en cada columna la propiedad correspondiente
-        }
-
-        const column = document.createElement("td"); // creo última columna
-        newRow.appendChild(column); // añado la columna a la fila correspondiente
-        const removeButton = document.createElement("button"); // creo el botón de eliminar
-        column.appendChild(removeButton); // añado el botón a la columna correspondiente
-        removeButton.textContent = "Remove"; // doy nombre al botón
-        removeButton.classList.add("btn", "btn-danger"); // doy las clases del botón de Bootstrap
-        removeButton.addEventListener("click", () => removeBook(book)); // creo el manejador de click y elimino el libro
-    }
-}
-updateTable(); // pintado de la tabla inicial
-
-const bookForm = document.getElementById("bookForm"); 
-
-function addNewBook() {
-    const id = books.length + 1; // id del libro n y le añado el siguiente n + 1
-    const title = document.getElementById("title");
-    const author = document.getElementById("author");
-    const sales = document.getElementById("sales");
-    const price = document.getElementById("price");
-    books.push(new Book(id, title.value, author.value, sales.value, price.value)); // añado el libro nuevo al array
-    updateTable(); // vuelvo a pintar la tabla
-    bookForm.reset(); // vacío los inputs
-}
+  
--- Tema_5_JavaScript/ejercicios/ejercicio6/js/eje_6.js
+++ Tema_5_JavaScript/ejercicios/ejercicio6/js/eje_6.js
@@ -5,7 +5,6 @@
 // 2. Queremos que para cada post vaya generando un h2 para cada p.
 
 // 3. Paginar de 20 en 20 con un botón de anterior y otro de siguiente que actualice los artículos que se están mostrando
-// funcion que reciba un array, un tamaño de pg y a partir de ese array que te de los elementos que necesitas
 
 
 // Apartado 1
@@ -31,7 +30,7 @@ fetch(URL)
     .then(data => {
         fillList(data);
     })
-    .catch(error => console.log(error)); 
+    .catch(error => console.log(error));
 
 
 
@@ -42,6 +41,7 @@ const URL2 = "https://jsonplaceholder.typicode.com/posts";
 const divContainer = document.getElementById("container");
 
 function fillTitle(array) {
+    divContainer.innerHTML = '';
     array.slice(0, 20).forEach(element => {
         divContainer.innerHTML += `<h2>${element.title}</h2><p>${element.body}</p>`;
     });
@@ -57,9 +57,59 @@ fetch(URL2)
     .then(data => {
         fillTitle(data);
     })
-    .catch(error => console.log(error)); 
+    .catch(error => console.log(error));
 
 
+// Apartado 3
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+
+let currentPage = 0;
+let pageSize = 20;
+let lastPage; // no la inicializo porque aún no se cuántas páginas habrá
+
+function pagination(array) {
+    lastPage = Math.ceil(array.length / pageSize) - 1; // Math.ceil lo utilizo para redondear 
+    // por si hubiera elementos sobrantes que no quepan en una página.
+    const pageFrom = currentPage * pageSize; // calculo el extremo inferior de la página
+    const pageTo = (currentPage + 1) * pageSize; // calculo el extremo superior de la página
+    fillTitle(array.slice(pageFrom, pageTo)); // pinto la porción del array solicitado
+}
+
+function handlePaginationClick(e) { // cambio a la página deseada
+    if (e.target === previousButton) { // ¿se ha clicado el botón "anterior"?
+        if (currentPage !== 0) { // decremento la página si no estoy en la primera
+            currentPage--;
+        }
+    } else {
+        if (currentPage !== lastPage) { // aumento la página si no estoy en la última
+            currentPage++;
+        }
+    }
+    nextPage(); // pido la página a la API y la pinto
+}
+
+function nextPage() { 
+    
+    // encierro el fetch en una función porque lo voy a utilizar en dos ocasiones, al clicar
+    // siguiente y anterior
+
+    fetch(URL2)
+        .then(response => {
+            if (!response.ok) {
+                throw Error("Hay algún problema con la respuesta: " + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            pagination(data);
+        })
+        .catch(error => console.log(error));
+}
+
+previousButton.addEventListener("click", handlePaginationClick);
+nextButton.addEventListener("click", handlePaginationClick);
 
 
 
