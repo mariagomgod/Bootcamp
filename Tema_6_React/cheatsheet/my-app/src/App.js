90d26b793e25f8deeb6a1082c81b9c39beb27163
Maria Gomez <maria_gg86@yahoo.es>
2021-04-19 13:56:13 +0200

    instalación e introducción React

---
+++ Tema_6_React/cheatsheet/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Tema_6_React/cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Título</h3>
+            <small>16 de agosto de 2025</small>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, consectetur?</p>
+        </div>
+    )
+}
+
+// export default FirstComponent // alternativa
