54da1bcc4fafe79debce4cebdd4d1a1892b9ae4b
Maria Gomez <maria_gg86@yahoo.es>
2021-05-02 19:26:03 +0200

    ejercicio 7 react finalizado

--- Tema_6_React/ejercicios/ejercicio7/src/components/Footer.js
+++ Tema_6_React/ejercicios/ejercicio7/src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <ul className="navbar-nav mr-auto ul">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">QUIÉNES SOMOS<span class="sr-only">(current)</span></a>
+                        <a className="nav-link" href="#">QUIÉNES SOMOS<span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">AVISO LEGAL</a>
@@ -20,7 +20,7 @@ export default function Footer() {
                     <li className="nav-item">
                         <a className="nav-link" href="#">POLÍTICA DE PRIVACIDAD</a>
                     </li>
-                    <li clasNames="nav-item">
+                    <li className="nav-item">
                         <a className="nav-link" href="#">POLÍTICA DE COOKIES</a>
                     </li>
                     <li className="nav-item">
--- Tema_6_React/ejercicios/ejercicio7/src/components/UpcomingMovies.js
+++ Tema_6_React/ejercicios/ejercicio7/src/components/UpcomingMovies.js
@@ -4,14 +4,23 @@ import './UpcomingMovies.css';
 export default function UpcomingMovies() {
 
     const [upcomingList, setUpcomingList] = useState([]);
+    const [criteria, setCriteria] = useState(""); // criterios de búsqueda
+    const [currentPage, setCurrentPage] = useState(1); // página actual de resultados
+    const [totalPages, setTotalPages] = useState(0); // total de paǵinas para pintar la botonera de paginación
 
     useEffect(() => {
-        const API_UPCOMING = "https://api.themoviedb.org/3/movie/upcoming?api_key=e327fb2b95e51192cbf506af48e3ff2b";
-        fetch(API_UPCOMING)
+
+        const API_MOVIES = criteria ? // si el criterio no está vacío tengo que llamar al endpoint de búsqueda
+            `https://api.themoviedb.org/3/search/movie?api_key=e327fb2b95e51192cbf506af48e3ff2b&query=${criteria}&page=${currentPage}` :
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=e327fb2b95e51192cbf506af48e3ff2b&page=${currentPage}`;
+
+        fetch(API_MOVIES)
             .then(response => response.json())
-            .then(data => setUpcomingList(data.results.slice(0, 20))); // Actualizamos el estado de la lista 
-        // con los 20 primeros elementos.
-    }, [])
+            .then(data => {
+                setUpcomingList(data.results.slice(0, 20)); // Actualizamos el estado de la lista con los 20 primeros elementos.
+                setTotalPages(data.total_pages);
+            });
+    }, [criteria, currentPage])
 
     function formatDate(dateString) {
 
@@ -22,6 +31,20 @@ export default function UpcomingMovies() {
 
     return (
         <div className="upcoming">
+            <input className="search" type="text" placeholder="Search" onChange={e => setCriteria(e.target.value)} />
+            <div className="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+                <div className="btn-group mr-2" role="group" aria-label="First group">
+                    {
+                        Array(totalPages).map((elem, i) => {
+                            return (
+                                <button type="button" key={i + 1} class="btn btn-secondary" onClick={() => setCurrentPage(i + 1)}>{i + 1}</button>
+                            );
+                        })
+                    } {/* Creo un array con tantas posiciones vacías como se le indique en el constructor (totalPages).
+                          Los valores los descarto porque lo que importa es saber el índice de cada uno para pintar el botón con 
+                          dicho índice + 1. */}
+                </div>
+            </div>
             <ul className="list-group mb-3">
                 {upcomingList.map((movie, index) => {
                     return (
