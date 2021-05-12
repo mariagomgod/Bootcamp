3472c703687655a35f8da58bbbad0fd8b1f4a0ad
Maria Gomez <maria_gg86@yahoo.es>
2021-05-12 19:44:05 +0200

    ejercicio 1 nodejs finalizado

--- Node.js/Ejercicios/ejercicio1.js
+++ Node.js/Ejercicios/ejercicio1.js
@@ -5,19 +5,42 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Petición GET recibida correctamente" });
+    //res.json({ message: "Petición GET recibida correctamente" });
+    res.json({
+        "name": "Oliver",
+        "profesion": "Full Stack Developer",
+        "age": 25,
+        "address": {
+            "street": "Larios Street, 15",
+            "city": "Málaga"
+        }
+    });
 });
 
-app.get("/:id", (req, res) => {
+app.put("/:id", (req, res) => {
     let id = req.params.id;
-    res.json({ message: `Petición GET con parámetro : ${id}`});
+    res.json({ message: `Petición PUT con parámetro : ${id}`});
+});
+
+app.delete("/:id", (req, res) => {
+    let id = req.params.id;
+    res.json({ message: `Petición DELETE con parámetro : ${id}`});
 });
 
 app.post("/", (req, res) => {
     let body = req.body;
 
     if (body.username) {
-        res.status(200).json({message: `Recibido username: ${body.username}`});
+        //res.status(200).json({message: `Recibido username: ${body.username}`});
+        res.status(200).json({
+            "name": "Oliver",
+            "profesion": "Full Stack Developer",
+            "age": 25,
+            "address": {
+                "street": "Larios Street, 15",
+                "city": "Málaga"
+            }
+        });
         
     } else {
         res.status(400).json({ok: false, message: "El username es obligatorio"});
